5ec7d42795f18cc2462fb7f80d04b4ea3bb893b8
PKnittel <pknittel@users.noreply.github.com>
2018-03-26 19:15:31 +0200

    Change line indentation

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const path = require('path');
 const url = require('url');
 
 function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
-    // und Laden der index.html der App.
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'client/index.html'),
-      protocol: 'file:',
-      slashes: true,
-    }));
-  }
-  
-  app.on('ready', createWindow);
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // und Laden der index.html der App.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'client/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+}
+
+app.on('ready', createWindow);
